6c6dd374e07cffeee7e15228bd77eb339c0dce49
HEROgold <martijnwieringa28@gmail.com>
2024-03-04 13:38:50 +0100

    added cooldown system

--- test_live.py
+++ test_live.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import cv2
 import numpy as np
 import supervision as sv
@@ -5,18 +6,27 @@ from ultralytics import YOLO
 
 from database.tables import Room, Session, engine
 
-SHOW_VIDEO = False
+SHOW_VIDEO = True
+DETECT_COOLDOWN_PERIOD = 60
+
 model = YOLO("yolov8n.pt", verbose=False)
 bounding_box_annotator = sv.BoundingBoxAnnotator()
 label_annotator = sv.LabelAnnotator()
 selected_classes = [0] # see https://stackoverflow.com/a/77479465
+rooms_to_check = [0, 1, 2, 3]
+room_cooldown = {}
 
-rooms_to_check = [
-    0
-]
 
-def detect_room(room_number: int):
-    cap = cv2.VideoCapture(0)
+def detect_room(target_number: int):
+    """
+    Detects the amount of ppl in a given room/camera, and updates the corresponding database table
+    
+    Parameters
+    -----------
+    target_number: int
+        room or camera number to detect
+    """
+    cap = cv2.VideoCapture(target_number)
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -33,10 +43,10 @@ def detect_room(room_number: int):
     if detected_amount > 0:
         # Human detected
         with Session(engine) as session:
-            if room := session.query(Room).where(Room.id == room_number).first():
+            if room := session.query(Room).where(Room.id == target_number).first():
                 room.human_count = detected_amount
             else:
-                session.add(Room(id=room_number, human_count=detected_amount))
+                session.add(Room(id=target_number, human_count=detected_amount))
             session.commit()
 
     # Annotate the image
@@ -65,7 +75,40 @@ def detect_room(room_number: int):
 
         # Display the resulting frame
         cv2.imshow('frame', annotated_image)
+    
     cap.release()
 
-# When everything done, release the capture
-cv2.destroyAllWindows()
+
+def reset_cooldown():
+    """
+    Reset the cooldown variable to 0 in datetime
+    """
+    for target in rooms_to_check:
+        room_cooldown[target] = datetime.fromtimestamp(0)
+
+
+def on_cooldown(target: int):
+    return room_cooldown[target] + timedelta(seconds=DETECT_COOLDOWN_PERIOD) >= datetime.now()
+
+
+def set_cooldown(target: int):
+    room_cooldown[target] = datetime.now()
+
+
+def main() -> None:
+    reset_cooldown()
+    run = True
+
+    while run:
+        for target in rooms_to_check:
+            if on_cooldown(target):
+                continue
+            detect_room(target)
+            set_cooldown(target)
+
+    # When everything done, release the capture
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
