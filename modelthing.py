265507c4354daa6a6513b0501e47e3fa5d0a65d5
HEROgold <martijnwieringa28@gmail.com>
2024-05-17 09:41:53 +0200

    add openvino model for cpu speedups

---
+++ modelthing.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+# Generate the model
+
+# model = YOLO("yolov8n.pt")
+# model.export(format="openvino", imgsz=640)
+
+# model.export(format="openvino", imgsz=640, half=True)
+# model.export(format="openvino", imgsz=640, int8=True, data="coco128.yaml")
+
+
+# Run the model.
+openvino_model = YOLO("yolov8n_openvino_model/", task="detect")
+openvino_model.predict(source=1, show=True, imgsz=640)
